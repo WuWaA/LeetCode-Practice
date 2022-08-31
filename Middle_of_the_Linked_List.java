4a186deeb137497a31a3c334f1bfd86727514fc1
Just-Practice <wu.wa.a@hotmail.com>
2022-08-31 04:04:34 -0400

    Move helper class into question file

--- Middle_of_the_Linked_List.java
+++ Middle_of_the_Linked_List.java
@@ -24,3 +24,20 @@ public class Middle_of_the_Linked_List {
         System.out.println(middleNode(node1));
     }
 }
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
