0fc1c2ba311251c3de85e7f4425119c6c1e7447b
WuWaA <wu.wa.a@qq.com>
2022-09-01 19:54:42 -0400

    Solve "Two Sum" with non-optimal solution

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,9 +1,18 @@
 public class Two_Sum {
+
+    /**
+     * 
+     * Constraints: Only one valid answer exists.
+     * Thus we could assume some situation to cutting down the average time complexity
+     * Faced Problem: -1000 + 1000 = 0, thus cannot ignore number such as "nums[i] <= target"
+     * Worst time: O(N^2), Expected average time: O(1/4N^2)
+     * Runtime: 47 ms, faster than 46.21% of Java online submissions for Two Sum.
+     * Memory Usage: 44.9 MB, less than 65.14% of Java online submissions for Two Sum.
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int[] twoSum(int[] nums, int target) {
-        // Constraints: Only one valid answer exists.
-        // Thus we could assume some situation to cutting down the average time complexity
-        // Faced Problem: -1000 + 1000 = 0, thus cannot ignore number
-        // Worst time: O(N^2), Average time: O(1/4N^2)
         for (int i = 0; i < nums.length; i += 2) { // try half num1
                 for (int j = i + 1; j < nums.length; j += 2) { // try half num2
                     if (nums[i] + nums[j] == target)
