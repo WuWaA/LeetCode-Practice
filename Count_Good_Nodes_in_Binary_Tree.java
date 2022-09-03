8828a933d939d633ecab5301e30ab9706259c551
WuWaA <wu.wa.a@qq.com>
2022-09-03 00:05:43 -0400

    Solved using recursion

---
+++ Count_Good_Nodes_in_Binary_Tree.java
@@ -0,0 +1,43 @@
+/**
+ * Runtime: 2 ms, faster than 100.00% of Java online submissions for Count Good Nodes in Binary Tree.
+ * Memory Usage: 60.1 MB, less than 29.75% of Java online submissions for Count Good Nodes in Binary Tree.
+ */
+class Solution {
+
+    int counter = 0;
+
+    public int goodNodes(TreeNode root) {
+        recursion(root, Integer.MIN_VALUE);
+        return counter;
+    }
+
+    void recursion(TreeNode node, int max) {
+        if (node == null)
+            return;
+        if (node.val > max)
+            max = node.val;
+        if (node.val >= max)
+            counter += 1;
+        recursion(node.left, max);
+        recursion(node.right, max);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
