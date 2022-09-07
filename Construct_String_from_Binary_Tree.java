ab3d84283cb5c33c5558cf5b4197cc1548d56725
WuWaA <wu.wa.a@qq.com>
2022-09-06 21:47:24 -0400

    Solve daily problem

---
+++ Construct_String_from_Binary_Tree.java
@@ -0,0 +1,47 @@
+public class Construct_String_from_Binary_Tree {
+
+    public String tree2str(TreeNode root) {
+        StringBuilder s = new StringBuilder();
+        DFS(root, s);
+        return s.toString();
+    }
+
+    void DFS(TreeNode r, StringBuilder s) {
+        if (r == null)
+            return;
+
+        s.append(r.val);
+
+        if (r.left != null) {
+            DFS(r.left, s.append("("));
+            s.append(")");
+        }
+
+        if (r.left == null && r.right != null)
+            s.append("()");
+
+        if (r.right != null) {
+            DFS(r.right, s.append("("));
+            s.append(")");
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
