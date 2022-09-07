9feeddb0176527ebbfe84cba3a45bff74b18af3f
WuWaA <wu.wa.a@qq.com>
2022-09-06 21:50:58 -0400

    Add comments

--- Construct_String_from_Binary_Tree.java
+++ Construct_String_from_Binary_Tree.java
@@ -1,5 +1,11 @@
 public class Construct_String_from_Binary_Tree {
 
+    /**
+     * Runtime: 1 ms, faster than 100.00% of Java online submissions for Construct String from Binary Tree.
+     * Memory Usage: 41.5 MB, less than 100.00% of Java online submissions for Construct String from Binary Tree.
+     * @param root
+     * @return
+     */
     public String tree2str(TreeNode root) {
         StringBuilder s = new StringBuilder();
         DFS(root, s);
@@ -17,6 +23,7 @@ public class Construct_String_from_Binary_Tree {
             s.append(")");
         }
 
+        // If node has right child, then left parenthesis are necessary
         if (r.left == null && r.right != null)
             s.append("()");
 
